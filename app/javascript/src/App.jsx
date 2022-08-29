946e808c76e5c3ee19a983429b148b505e10ccb4
mr-jestin-roy <roy_jestin@yahoo.com>
2022-08-29 13:59:36 +0530

    Set up react environment

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,7 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
+// previous code if any
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  // previous code if any
+
+  useEffect(() => {
+    initializeLogger();
+    setAuthHeaders(setLoading);
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  // previous code without changes
   return (
     <Router>
       <Switch>
