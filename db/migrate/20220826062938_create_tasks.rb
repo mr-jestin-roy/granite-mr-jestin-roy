1615e10a33eccbdd8b58edcf587a02cf46f40c6b
mr-jestin-roy <roy_jestin@yahoo.com>
2022-08-29 18:59:53 +0530

    Added tasks controller and index action

--- db/migrate/20220826062938_create_tasks.rb
+++ db/migrate/20220826062938_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.text :body
+      t.text :title
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_08_26_062938) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "body"
+    t.text "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
