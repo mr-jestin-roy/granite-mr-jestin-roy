3c4c2b86a2072c2462af1c939f03fa9ca617fdf9
mr-jestin-roy <roy_jestin@yahoo.com>
2022-08-30 13:50:13 +0530

    Error in API based architecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks, only: :index
+
+  root "home#index"
+  get "*path", to: "home#index", via: :all
 end
