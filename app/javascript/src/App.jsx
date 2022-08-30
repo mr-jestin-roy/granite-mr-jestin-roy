52ddcffc626077e698715b68b63220d6dbd4abd7
mr-jestin-roy <roy_jestin@yahoo.com>
2022-08-30 10:53:21 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
 // previous code if any
--- babel.config.js
+++ babel.config.js
@@ -21,11 +21,11 @@ module.exports = function (api) {
         "@babel/preset-env",
         {
           targets: {
-            node: "current",
+            node: "current"
           },
-          modules: "commonjs",
+          modules: "commonjs"
         },
-        "@babel/preset-react",
+        "@babel/preset-react"
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         "@babel/preset-env",
@@ -34,67 +34,57 @@ module.exports = function (api) {
           useBuiltIns: "entry",
           corejs: 3,
           modules: false,
-          exclude: ["transform-typeof-symbol"],
-        },
+          exclude: ["transform-typeof-symbol"]
+        }
       ],
       [
         "@babel/preset-react",
         {
           development: isDevelopmentEnv || isTestEnv,
-          useBuiltIns: true,
-        },
-      ],
+          useBuiltIns: true
+        }
+      ]
     ].filter(Boolean),
     plugins: [
-      "js-logger",
+      ["@babel/plugin-proposal-private-methods", { loose: true }],
+      ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
       "babel-plugin-macros",
       "@babel/plugin-syntax-dynamic-import",
+      "js-logger",
       isTestEnv && "babel-plugin-dynamic-import-node",
       "@babel/plugin-transform-destructuring",
       [
         "@babel/plugin-proposal-class-properties",
         {
-          loose: true,
-        },
+          loose: true
+        }
       ],
       [
         "@babel/plugin-proposal-object-rest-spread",
         {
-          useBuiltIns: true,
-        },
-      ],
-      [
-        "@babel/plugin-proposal-private-methods",
-        {
-          loose: true,
-        },
-      ],
-      [
-        "@babel/plugin-proposal-private-property-in-object",
-        {
-          loose: true,
-        },
+          useBuiltIns: true
+        }
       ],
       [
         "@babel/plugin-transform-runtime",
         {
           helpers: false,
           regenerator: true,
-          corejs: false,
-        },
+          corejs: false
+        }
       ],
       [
         "@babel/plugin-transform-regenerator",
         {
-          async: false,
-        },
+          async: false
+        }
       ],
       isProductionEnv && [
         "babel-plugin-transform-react-remove-prop-types",
         {
-          removeImport: true,
-        },
-      ],
-    ].filter(Boolean),
+          removeImport: true
+        }
+      ]
+    ].filter(Boolean)
   };
 };
