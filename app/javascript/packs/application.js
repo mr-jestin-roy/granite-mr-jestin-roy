814e8de0111eac030b33da477eae88f9675a0196
mr-jestin-roy <roy_jestin@yahoo.com>
2022-08-30 10:24:18 +0530

    Added React.js to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,10 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-import Rails from "@rails/ujs";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
 import "../stylesheets/application.scss";
 
-Rails.start();
-ActiveStorage.start();
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("channels");
+
+var componentRequireContext = require.context("src", true);
+var ReactRailsUJS = require("react_ujs");
+
+ReactRailsUJS.useContext(componentRequireContext);
