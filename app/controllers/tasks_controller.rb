f11ed80fb5130cab3ae9d307bc87fe2b48e98340
mr-jestin-roy <roy_jestin@yahoo.com>
2022-09-01 15:34:44 +0530

    Added ability to assign task to a user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,8 @@
 class TasksController < ApplicationController
   before_action :load_task!, only: %i[show update destroy]
   def index
-    tasks = Task.all
-    render status: :ok, json: { tasks: tasks }
+    tasks = Task.all.as_json(include: { assigned_user: { only: %i[name id] } })
+    respond_with_json({ tasks: tasks })
   end
 
   def create
@@ -14,14 +14,14 @@ class TasksController < ApplicationController
   end
 
   def show
-    respond_with_json({ task: @task })
-  end
+    respond_with_json({ task: @task, assigned_user: @task.assigned_user })
+end
 
   def update
     task = Task.find_by!(slug: params[:slug])
     task.update!(task_params)
     respond_with_success(t("successfully_updated"))
-end
+  end
 
   def destroy
     @task.destroy!
@@ -30,11 +30,11 @@ end
 
   private
 
-    def load_task!
-      @task = Task.find_by!(slug: params[:slug])
+    def task_params
+      params.require(:task).permit(:title, :assigned_user_id)
     end
 
-    def task_params
-      params.require(:task).permit(:title)
+    def load_task!
+      @task = Task.find_by!(slug: params[:slug])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   resources :tasks, except: %i[new edit], param: :slug
-
+  resources :users, only: :index
   root "home#index"
   get "*path", to: "home#index", via: :all
 end
