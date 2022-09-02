a7526385fb8d127c5dd5bb831545e1755612c38f
mr-jestin-roy <roy_jestin@yahoo.com>
2022-09-02 17:04:25 +0530

    post test changes

--- z-BBA Assesment/2nd Test/JS-2.js
+++ z-BBA Assesment/2nd Test/JS-2.js
@@ -1,10 +1,8 @@
 function snakeToPascal(str) {
-  str += "";
-  str = str.split("_");
-  for (var i = 0; i < str.length; i++) {
-    str[i] = str[i].slice(0, 1).toUpperCase() + str[i].slice(1, str[i].length);
-  }
-  return str.join("");
+  return str
+    .split("_")
+    .map(s => (s = s.charAt(0).toUpperCase() + s.slice(1)))
+    .join("");
 }
 const string = "admin_post_big_binary";
 console.log(snakeToPascal(string));
