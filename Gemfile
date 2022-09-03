950b93237b5a1ba617b52843a694bc2e01ff8543
mr-jestin-roy <roy_jestin@yahoo.com>
2022-09-03 12:13:39 +0530

    Added has_secure_password and validations to User model

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem "jbuilder", "~> 2.7"
 # gem 'bcrypt', '~> 3.1.7'
 gem "react-rails"
 
+gem "bcrypt", "~> 3.1.13"
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  MAX_EMAIL_LENGTH = 255
+
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+  has_secure_password
 
   validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: MAX_EMAIL_LENGTH },
+    format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }, if: -> { password.present? }
+  validates :password_confirmation, presence: true, on: :create
+
+  before_save :to_lowercase
+
+  private
+
+    def to_lowercase
+      email.downcase!
+    end
 end
