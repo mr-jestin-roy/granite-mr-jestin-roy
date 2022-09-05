acd0a407fe332737b515ee8d81c63143ce3b890a
mr-jestin-roy <roy_jestin@yahoo.com>
2022-09-05 17:45:04 +0530

    Added controller tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,4 +29,12 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
   # Add more helper methods to be used by all tests here...
+  def headers(user, options = {})
+    {
+      Accept: "application/json",
+      "Content_Type" => "application/json",
+      "X-Auth-Token" => user.authentication_token,
+      "X-Auth-Email" => user.email
+    }.merge(options)
+  end
 end
