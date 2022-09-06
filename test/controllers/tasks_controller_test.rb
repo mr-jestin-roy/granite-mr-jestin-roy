afd763dc7300cc3190490a3028a027bca39e4da6
mr-jestin-roy <roy_jestin@yahoo.com>
2022-09-06 17:02:50 +0530

    fixed the session and task controller test

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -76,6 +76,6 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     get task_path(invalid_slug), headers: @creator_headers
     assert_response :not_found
     response_json = response.parsed_body
-    assert_equal response_json["error"], t("not_found", entity: "Task")
+    assert_equal response_json["error"], t("Couldn't find Task", entity: "Task")
   end
 end
